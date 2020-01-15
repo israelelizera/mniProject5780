4cbfe3567a98efb1d6bbdf9814301a35370ede85
israel elizera <elizera654@gmail.com>
2020-01-15 16:53:13 +0200

    WPF

--- PLWPF/AddGuestRequest.xaml.cs
+++ PLWPF/AddGuestRequest.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BL;
 
 namespace PLWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddGuestRequest.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddGuestRequest : Window
     {
-        public MainWindow()
+        public AddGuestRequest()
         {
             InitializeComponent();
-            IBL myBL = new BL_imp();
         }
     }
 }
