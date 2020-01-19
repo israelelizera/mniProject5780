8528032da0ca2c4190c6e1a0899f1ca7aa753abd
israel elizera <elizera654@gmail.com>
2020-01-19 14:43:04 +0200

    dc

--- PLWPF/AddGuestRequest.xaml.cs
+++ PLWPF/AddGuestRequest.xaml.cs
@@ -15,6 +15,14 @@ namespace PLWPF
             InitializeComponent();
             guestRequest = new BE.GuestRequest();
             GuestRequestDetails.DataContext = guestRequest;
+            guestRequest.PrivateName=(this.PrivateNameBox.Text);
+            this.ChildrenAttractionBox.ItemsSource = Enum.GetValues(typeof(BE.Request));
+            this.GardenBox.ItemsSource = Enum.GetValues(typeof(BE.Request));
+            this.JacuzziBox.ItemsSource = Enum.GetValues(typeof(BE.Request));
+            this.LocationBox.ItemsSource = Enum.GetValues(typeof(BE.Location));
+            this.PoolBox.ItemsSource = Enum.GetValues(typeof(BE.Request));
+            this.TypeBox.ItemsSource = Enum.GetValues(typeof(BE.KindOfUnit));
+
             bL = new BL.BL_imp();
         }
 
